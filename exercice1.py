b15ad018f3f1ff767adf086442d2561b7d02b7a7
volonte malisava <volontemuhi@gmail.com>
2026-03-30 14:57:37 +0200

    chiffrement

--- exercice1.py
+++ exercice1.py
@@ -1,2 +1,15 @@
-# nous allons utiliser les deux bibliotheque(cryptography et l'algoritheme fernet) pour chiffrer et dechiffrer un message
 from cryptography.fernet import Fernet
+
+message_clair = b"Bonjour, ceci est un message secret!"
+
+#generer une cle de chiffrement
+cle_de_chiffrement = Fernet.generate_key()
+
+#charger l'algorithme de chiffrement 
+chiffrement=Fernet(cle_de_chiffrement)
+
+#chiffrement du message
+message_chiffre = chiffrement.encrypt(message_clair)
+
+print("Message clair:", message_clair)
+print("Message chiffré:", message_chiffre)
